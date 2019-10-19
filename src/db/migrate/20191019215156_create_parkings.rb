7fc0ba37bd15d88b24f91b457cfcc28fc5a89570
Felipe Rian <feliperian@gmail.com>
2019-10-19 18:53:21 -0300

    Parking model created

---
+++ src/db/migrate/20191019215156_create_parkings.rb
@@ -0,0 +1,12 @@
+class CreateParkings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parkings do |t|
+      t.string :plate
+      t.datetime :input
+      t.datetime :output
+      t.boolean :paid
+
+      t.timestamps
+    end
+  end
+end
