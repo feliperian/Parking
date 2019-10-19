d70a6cbada6c5772d2febe65068d9496f5331b15
Felipe Rian <feliperian@gmail.com>
2019-10-19 19:10:40 -0300

    API tests are inserted

---
+++ src/spec/requests/paking_spec.rb
@@ -0,0 +1,26 @@
+# spec/requests/todos_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Parking API', type: :request do
+  # initialize test data 
+
+  # Test suite for POST /parking
+  describe 'POST /parking' do
+    pending "this needs to be implemented"
+  end
+
+  # Test suite for PUT /parking/:id/out
+  describe 'PUT /parking/:id/out' do
+    pending "this needs to be implemented"
+  end
+
+  # Test suite for PUT /parking/:id/pay
+  describe 'PUT /parking/:id/pay' do
+    pending "this needs to be implemented"
+  end
+
+  # Test suite for GET /parking/:plate
+  describe 'GET /parking/:plate' do
+    pending "this needs to be implemented"
+  end
+end
