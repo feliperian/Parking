73e66364481cad35253b742a42f766230be75c89
Felipe Rian <feliperian@gmail.com>
2019-10-19 14:29:21 -0300

    Removed hyphen in parking-api resulting in parkingapi

--- init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE parking-api_development;
-    CREATE DATABASE parking-api_test;
-    CREATE DATABASE parking-api_production;
-EOSQL
